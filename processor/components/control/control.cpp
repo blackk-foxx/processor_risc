c078b2a0acf53e4a3616c529bfbcfcae6bb7e839
Todd Schwartz <todd@tlschwartz.com>
2023-04-30 01:30:43 +0000

    Replace opcode if/else with map

--- processor/components/control/control.cpp
+++ processor/components/control/control.cpp
@@ -10,129 +10,149 @@
  * **/
 SC_MODULE(control) {
 
-	public:
-		//-- Signals --//
-		//-- In --//
-		sc_in_clk clock;
-		sc_in<sc_uint<4> > opcode;	// OPCODE
-		sc_in<sc_uint<8> > of1;		// Operando fonte 1
-		sc_in<sc_uint<8> > of2;		// Operando fonte 2
-		sc_in<sc_uint<9> > opd;		// Operando destino
-		sc_in<bool> N;
-		sc_in<bool> Z;
+public:
+    //-- Signals --//
+    //-- In --//
+    sc_in_clk clock;
+    sc_in<sc_uint<4> > opcode;	// OPCODE
+    sc_in<sc_uint<8> > of1;		// Operando fonte 1
+    sc_in<sc_uint<8> > of2;		// Operando fonte 2
+    sc_in<sc_uint<9> > opd;		// Operando destino
+    sc_in<bool> N;
+    sc_in<bool> Z;
 
-		//-- Out --//
-		// Program counter control
-		sc_out<bool> enableCP;
-		sc_out<bool> loadCP;
-		sc_out<bool> resetCP;
-		sc_out<sc_uint<9>> jumpValueCP;
+    //-- Out --//
+    // Program counter control
+    sc_out<bool> enableCP;
+    sc_out<bool> loadCP;
+    sc_out<bool> resetCP;
+    sc_out<sc_uint<9>> jumpValueCP;
 
-		// Instruction memory control
-		sc_out<bool> enableIM;
-		sc_out<bool> writeIM;
+    // Instruction memory control
+    sc_out<bool> enableIM;
+    sc_out<bool> writeIM;
 
-		// Data memory control;
-		sc_out<bool> enableDM;
-		sc_out<bool> writeDM;
+    // Data memory control;
+    sc_out<bool> enableDM;
+    sc_out<bool> writeDM;
 
-		// Instruction register control
-		sc_out<bool> enableRI;
-		sc_out<bool> writeRI;
-		sc_out<sc_uint<8>> immediateValue;
-		sc_out<sc_uint<9>> immediateRegister;
+    // Instruction register control
+    sc_out<bool> enableRI;
+    sc_out<bool> writeRI;
+    sc_out<sc_uint<8>> immediateValue;
+    sc_out<sc_uint<9>> immediateRegister;
 
-		// Register base control
-		sc_out<bool> enableRB;
-		sc_out<bool> writeRB;
+    // Register base control
+    sc_out<bool> enableRB;
+    sc_out<bool> writeRB;
 
-		// Register pipeline
-		sc_out<bool> enableRPL;
-		sc_out<bool> writeRPL;
-		sc_out<bool> resetRPL;
+    // Register pipeline
+    sc_out<bool> enableRPL;
+    sc_out<bool> writeRPL;
+    sc_out<bool> resetRPL;
 
-		// ULA control
-		sc_out<bool> resetZN;
+    // ULA control
+    sc_out<bool> resetZN;
 
-		// Multiplex for write data on register base
-		sc_out<sc_uint<2>> seletorMultiRBW;
+    // Multiplex for write data on register base
+    sc_out<sc_uint<2>> seletorMultiRBW;
 
-		// Multiplex for addressing DM
-		sc_out<sc_uint<2>> seletorMultiDM;
-	
-		//-- Constructor --//
-		SC_CTOR(control) {
-			SC_METHOD(state_machine);
-			sensitive << clock.pos();
-		}	
+    // Multiplex for addressing DM
+    sc_out<sc_uint<2>> seletorMultiDM;
 
-        unsigned getCycleCount() const { return cycleCount; }
+    //-- Constructor --//
+    SC_CTOR(control) {
+        SC_METHOD(state_machine);
+        sensitive << clock.pos();
+    }	
 
-	private:
-		//-- Local variables --//
-        void (control::*state)() = &control::state_INITIAL;
-		bool restartPipe = false;
-        unsigned cycleCount = 0;
+    unsigned getCycleCount() const { return cycleCount; }
 
-		//-- Methods for making pipeline easy --//
+private:
+    //-- Local variables --//
+    void (control::*state)() = &control::state_INITIAL;
+    bool restartPipe = false;
+    unsigned cycleCount = 0;
 
-		/*
-		 * Prepare components so that, in the next cycle,
-		 * the instruction will be available.
-		 * * */
-		void prepareReadInstFromIM();
+    typedef enum opcode_t {
+        HALT = 0,
+        J = 10,
+        JN = 11,
+        JZ = 12,
+        LD = 8,
+        LRI = 13,
+        ST = 9,
+    };
 
-		/* Prepare components so that the instruction
-		 * will be loaded into the instruction register in
-		 * the next cycle.
-		 * * */
-		void prepareLoadRI();
+    const std::map<unsigned, void(control::*)()> handler_for_opcode = {
+        {opcode_t::HALT, &control::handle_HALT},
+        {opcode_t::J, &control::handle_J},
+        {opcode_t::JN, &control::handle_JN},
+        {opcode_t::JZ, &control::handle_JZ},
+        {opcode_t::LD, &control::handle_LD},
+        {opcode_t::LRI, &control::handle_LRI},
+        {opcode_t::ST, &control::handle_ST},
+    };
 
-		/* Prepare components so that the decoded instruction
-		 * will be loaded into the pipeline register.
-		 * **/
-		void prepareLoadRPL();
+    //-- Methods for making pipeline easy --//
 
-		/* Make the instruction waits when reaches the cycle
-		 * before the pipeline register (for pipeline purposes
-		 * only).
-		 * **/
-		void stallPipe();
+    /*
+     * Prepare components so that, in the next cycle,
+     * the instruction will be available.
+     * * */
+    void prepareReadInstFromIM();
 
-        void prepareJump();
-        void prepareConditionalJump();
-        void prepareReadRB();
-        void prepareWriteRB();
-        void prepareReadDM();
-        void prepareWriteDM();
+    /* Prepare components so that the instruction
+     * will be loaded into the instruction register in
+     * the next cycle.
+     * * */
+    void prepareLoadRI();
 
-        void handleOpcode(unsigned opcode);
-        void handle_LRI();
-        void handle_LD();
-        void handle_ST();
-        void handle_J();
-        void handle_JN();
-        void handle_JZ();
-        void handle_HALT();
-        void handle_ALU_operation();
+    /* Prepare components so that the decoded instruction
+     * will be loaded into the pipeline register.
+     * **/
+    void prepareLoadRPL();
 
-		/* The Mealy's state machine: uses the current
-		 * state and informations from outside in order
-		 * to keep the processor working properly in
-		 * terms of microinstructions.
-		 * **/
-		void state_machine();
+    /* Make the instruction waits when reaches the cycle
+     * before the pipeline register (for pipeline purposes
+     * only).
+     * **/
+    void stallPipe();
 
-        void state_INITIAL();
-        void state_INST_READY();
-        void state_IR_HAS_INST();
-        void state_RPL_READY();
-        void state_READY_TO_EXECUTE();
-        void state_READY_TO_COMPUTE();
-        void state_READY_TO_STORE();
-        void state_READY_TO_JUMP();
-        void state_RESULT_READY();
-        void state_READY_TO_LOAD();
+    void prepareJump();
+    void prepareConditionalJump();
+    void prepareReadRB();
+    void prepareWriteRB();
+    void prepareReadDM();
+    void prepareWriteDM();
+
+    void handleOpcode(unsigned opcode);
+    void handle_LRI();
+    void handle_LD();
+    void handle_ST();
+    void handle_J();
+    void handle_JN();
+    void handle_JZ();
+    void handle_HALT();
+    void handle_ALU_operation();
+
+    /* The Mealy's state machine: uses the current
+     * state and informations from outside in order
+     * to keep the processor working properly in
+     * terms of microinstructions.
+     * **/
+    void state_machine();
+
+    void state_INITIAL();
+    void state_INST_READY();
+    void state_IR_HAS_INST();
+    void state_RPL_READY();
+    void state_READY_TO_EXECUTE();
+    void state_READY_TO_COMPUTE();
+    void state_READY_TO_STORE();
+    void state_READY_TO_JUMP();
+    void state_RESULT_READY();
+    void state_READY_TO_LOAD();
 
 };
 
@@ -167,19 +187,18 @@ void control::state_RPL_READY() {
     state = &control::state_READY_TO_EXECUTE;
 }
 
-enum opcode {
-    HALT = 0,
-    J = 10,
-    JN = 11,
-    JZ = 12,
-    LD = 8,
-    LRI = 13,
-    ST = 9,
-};
-
 void control::state_READY_TO_EXECUTE() {
     prepareLoadRI(); // Put new instruction on RI (pipeline)
     handleOpcode(opcode.read());
+}
+
+void control::handleOpcode(unsigned opcode)
+{
+    auto handler = handler_for_opcode.find(opcode);
+    if (handler != handler_for_opcode.cend())
+        (*this.*handler->second)();
+    else
+        handle_ALU_operation();
 }
 
 void control::handle_LRI()
@@ -215,17 +234,15 @@ void control::handle_J()
 
 void control::handle_JN()
 {
-    if (N.read() == 1) {
+    if (N.read() == 1)
         prepareConditionalJump();
-    }
     state = &control::state_READY_TO_JUMP;
 }
 
 void control::handle_JZ()
 {
-    if (Z.read() == 1) {
+    if (Z.read() == 1)
         prepareConditionalJump();
-    }
     state = &control::state_READY_TO_JUMP;
 }
 
@@ -239,27 +256,6 @@ void control::handle_ALU_operation()
     seletorMultiRBW.write(0);
     prepareReadRB();
     state = &control::state_READY_TO_COMPUTE;
-}
-
-void control::handleOpcode(unsigned opcode) 
-{
-    if (opcode == opcode::LRI) {
-        handle_LRI();
-    } else if (opcode == opcode::LD) {
-        handle_LD();
-    } else if (opcode == opcode::ST) {
-        handle_ST();
-    } else if (opcode == opcode::J) {
-        handle_J();
-    } else if (opcode == opcode::JN) {
-        handle_JN();
-    } else if (opcode == opcode::JZ) {
-        handle_JZ();
-    } else if (opcode == opcode::HALT) {
-        handle_HALT();
-    } else {
-        handle_ALU_operation();
-    }
 }
 
 void control::state_READY_TO_COMPUTE() {
